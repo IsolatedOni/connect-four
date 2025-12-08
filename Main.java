94f6187750dd24aa8a97dc9e0238004179d28f98
IsolatedOni <159647406+IsolatedOni@users.noreply.github.com>
2025-12-07 21:25:04 -0500

    Create Main.java
    Created  the main class with the basic coin flip and the gameCond boolean to end the game after the computer or player wins.

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main{
+  public static void main(String[] args){
+
+    int coin = (int) ((Math.random() * 2) + 1);
+
+    boolean gameCond;
+
+    while(gameCond == false){
+
+    }
+  }
+}
